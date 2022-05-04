a9d6a0594c599e17e79b7fdf8757f4c123519d02
indigobluw <lindquist20@gmail.com>
2022-05-04 22:04:11 +0200

    added js file

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+document.getElementById("hero-btn").addEventListener("click", function () {
+  document.querySelector(".background-popup").style.display = "flex";
+});
+
+document.getElementsById("close-btn").addEventListener("click", function () {
+  document.querySelector(".background-popup").style.display = "none";
+});
+
+document.getElementsById("login-btn").addEventListener("click", function () {
+  document.querySelector(".background-popup").style.display = "none";
+});
