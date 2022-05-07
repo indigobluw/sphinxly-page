dabe0b9dc9b8a2071eced00e12b8df1e3db59bd4
indigobluw <lindquist20@gmail.com>
2022-05-07 15:47:53 +0200

    close button works

--- fungerar.js
+++ fungerar.js
@@ -1,4 +1,4 @@
-document.getElementById("hero-btn").addEventListener("click", function () {
+/*document.getElementById("hero-btn").addEventListener("click", function () {
     document.querySelector(".background-popup").style.display = "flex";
   });
   
@@ -19,5 +19,30 @@ document.getElementById("hero-btn").addEventListener("click", function () {
     } else {
       hbtn.innerText = "LOGGA IN";
     }
-  });
+  });*/
+
+
+
+
+
+
+
+  const hbtn = document.getElementById("hero-btn");
+const lbtn = document.getElementById("login-btn");
+
+hbtn.addEventListener("click", function () {
+  if (hbtn.innerText === "LOGGA IN") {
+    document.querySelector(".background-popup").style.display = "flex";
+  } else if (hbtn.innerText === "LOGGA UT") {
+    hbtn.innerText = "LOGGA IN";
+  }
+});
+
+lbtn.addEventListener("click", function () {
+  if (lbtn.innerText === "Logga in") {
+    document.querySelector(".background-popup").style.display = "none";
+    hbtn.innerText = "LOGGA UT";
+  }
+});
+
   
--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,12 @@
 const hbtn = document.getElementById("hero-btn");
 const lbtn = document.getElementById("login-btn");
+const cbtn = document.getElementById("close-btn");
 
 hbtn.addEventListener("click", function () {
   if (hbtn.innerText === "LOGGA IN") {
     document.querySelector(".background-popup").style.display = "flex";
   } else if (hbtn.innerText === "LOGGA UT") {
+    document.querySelector(".login-confirm").style.display = "none";
     hbtn.innerText = "LOGGA IN";
   }
 });
@@ -12,6 +14,11 @@ hbtn.addEventListener("click", function () {
 lbtn.addEventListener("click", function () {
   if (lbtn.innerText === "Logga in") {
     document.querySelector(".background-popup").style.display = "none";
+    document.querySelector(".login-confirm").style.display = "flex";
     hbtn.innerText = "LOGGA UT";
   }
+});
+
+cbtn.addEventListener("click", function () {
+  document.querySelector(".background-popup").style.display = "none";
 });
