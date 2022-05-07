5c23bd8b6858a8c1216c8303cefcd021bfcc26d5
indigobluw <lindquist20@gmail.com>
2022-05-07 16:41:26 +0200

    namn input fungerar

--- myscripts.js
+++ myscripts.js
@@ -2,11 +2,16 @@ const hbtn = document.getElementById("hero-btn");
 const lbtn = document.getElementById("login-btn");
 const cbtn = document.getElementById("close-btn");
 
+const nameInput = document.querySelector(".nameInput");
+const result = document.querySelector(".result");
+const lbtns = document.querySelector(".login-btn");
+
 hbtn.addEventListener("click", function () {
   if (hbtn.innerText === "LOGGA IN") {
     document.querySelector(".background-popup").style.display = "flex";
   } else if (hbtn.innerText === "LOGGA UT") {
     document.querySelector(".login-confirm").style.display = "none";
+    document.querySelector(".result").style.display = "none";
     hbtn.innerText = "LOGGA IN";
   }
 });
@@ -15,6 +20,7 @@ lbtn.addEventListener("click", function () {
   if (lbtn.innerText === "Logga in") {
     document.querySelector(".background-popup").style.display = "none";
     document.querySelector(".login-confirm").style.display = "flex";
+    document.querySelector(".result").style.display = "flex";
     hbtn.innerText = "LOGGA UT";
   }
 });
@@ -22,3 +28,8 @@ lbtn.addEventListener("click", function () {
 cbtn.addEventListener("click", function () {
   document.querySelector(".background-popup").style.display = "none";
 });
+
+nameInput.addEventListener(
+  "input",
+  (letter) => (result.textContent = letter.target.value)
+);
