0f2545f07f34f9dfa80115905078960e6cf19f36
indigobluw <lindquist20@gmail.com>
2022-05-07 11:43:20 +0200

    working if statement

--- myscripts.js
+++ myscripts.js
@@ -4,10 +4,19 @@ document.getElementById("hero-btn").addEventListener("click", function () {
 
 document.querySelector(".close-btn").addEventListener("click", function () {
   document.querySelector(".background-popup").style.display = "none";
-  console.log();
 });
 
 document.querySelector(".login-btn").addEventListener("click", function () {
   document.querySelector(".background-popup").style.display = "none";
-  console.log();
+});
+
+const hbtn = document.getElementById("hero-btn");
+const lbtn = document.getElementById("login-btn");
+
+lbtn.addEventListener("click", () => {
+  if (hbtn.innerText === "LOGGA IN") {
+    hbtn.innerText = "LOGGA UT";
+  } else {
+    hbtn.innerText = "LOGGA IN";
+  }
 });
