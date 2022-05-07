e75974daf99636590ebc7920b6c00d66079478ef
indigobluw <lindquist20@gmail.com>
2022-05-07 14:21:55 +0200

    knapp logga in och logga ut fungerar

---
+++ fungerar.js
@@ -0,0 +1,23 @@
+document.getElementById("hero-btn").addEventListener("click", function () {
+    document.querySelector(".background-popup").style.display = "flex";
+  });
+  
+  document.querySelector(".close-btn").addEventListener("click", function () {
+    document.querySelector(".background-popup").style.display = "none";
+  });
+  
+  document.querySelector(".login-btn").addEventListener("click", function () {
+    document.querySelector(".background-popup").style.display = "none";
+  });
+  
+  const hbtn = document.getElementById("hero-btn");
+  const lbtn = document.getElementById("login-btn");
+  
+  lbtn.addEventListener("click", () => {
+    if (hbtn.innerText === "LOGGA IN") {
+      hbtn.innerText = "LOGGA UT";
+    } else {
+      hbtn.innerText = "LOGGA IN";
+    }
+  });
+  
--- myscripts.js
+++ myscripts.js
@@ -1,22 +1,17 @@
-document.getElementById("hero-btn").addEventListener("click", function () {
-  document.querySelector(".background-popup").style.display = "flex";
-});
-
-document.querySelector(".close-btn").addEventListener("click", function () {
-  document.querySelector(".background-popup").style.display = "none";
-});
-
-document.querySelector(".login-btn").addEventListener("click", function () {
-  document.querySelector(".background-popup").style.display = "none";
-});
-
 const hbtn = document.getElementById("hero-btn");
 const lbtn = document.getElementById("login-btn");
 
-lbtn.addEventListener("click", () => {
+hbtn.addEventListener("click", function () {
   if (hbtn.innerText === "LOGGA IN") {
-    hbtn.innerText = "LOGGA UT";
-  } else {
+    document.querySelector(".background-popup").style.display = "flex";
+  } else if (hbtn.innerText === "LOGGA UT") {
     hbtn.innerText = "LOGGA IN";
+  }
+});
+
+lbtn.addEventListener("click", function () {
+  if (lbtn.innerText === "Logga in") {
+    document.querySelector(".background-popup").style.display = "none";
+    hbtn.innerText = "LOGGA UT";
   }
 });
