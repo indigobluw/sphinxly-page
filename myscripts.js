f19878b6cb85e1d3679f265beee22d5c53468d9d
indigobluw <lindquist20@gmail.com>
2022-05-10 15:50:39 +0200

    working compareson key value

--- myscripts.js
+++ myscripts.js
@@ -7,23 +7,24 @@ const lbtns = document.querySelector(".login-btn");
 var getItem = localStorage.getItem.innerText;
 
 hbtn.addEventListener("click", function () {
-    if (hbtn.innerText === "LOGGA IN") {
+  if (hbtn.innerText === "LOGGA IN") {
     document.querySelector(".background-popup").style.display = "flex";
   } else if (hbtn.innerText === "LOGGA UT") {
     document.querySelector(".login-confirm").style.display = "none";
+    document.querySelector(".login-again").style.display = "none";
     document.querySelector(".result").style.display = "none";
     hbtn.innerText = "LOGGA IN";
   }
-  });
+});
 
 const saveToLocalStorage = () => {
   localStorage.setItem("inputsName", result.textContent);
-  };
+};
 
 lbtn.addEventListener("click", saveToLocalStorage);
 
 lbtn.addEventListener("click", function () {
-    if (lbtn.innerText === "Logga in" && nameInput.innerText == localStorage.getItem.innerText) {
+  if (lbtn.innerText === "Logga in" && nameInput.value == localStorage.getItem("inputsName")) {
     document.querySelector(".background-popup").style.display = "none";
     document.querySelector(".login-again").style.display = "flex";
     document.querySelector(".result").style.display = "flex";
@@ -34,13 +35,13 @@ lbtn.addEventListener("click", function () {
     document.querySelector(".result").style.display = "flex";
     hbtn.innerText = "LOGGA UT";
   }
-  });
+});
 
 cbtn.addEventListener("click", function () {
   document.querySelector(".background-popup").style.display = "none";
-  });
+});
 
 nameInput.addEventListener(
   "input",
   (letter) => (result.textContent = letter.target.value)
-  );
+);
