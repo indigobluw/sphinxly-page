de312c0ea7fcc7fd317e03afac6653bcc87180d7
indigobluw <lindquist20@gmail.com>
2022-05-10 18:19:55 +0200

    saves input to array

--- myscripts.js
+++ myscripts.js
@@ -6,6 +6,21 @@ const result = document.querySelector(".result");
 const lbtns = document.querySelector(".login-btn");
 var getItem = localStorage.getItem.innerText;
 
+
+/*function save () {
+  var newData = document.getElementById("nameInput").value;
+  
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", "[]");
+  }
+
+  var oldData = JSON.parse(localStorage.getItem("data"));
+  oldData.push(newData);
+
+  localStorage.setItem("data", JSON.stringify(oldData));
+}*/
+
+
 hbtn.addEventListener("click", function () {
   if (hbtn.innerText === "LOGGA IN") {
     document.querySelector(".background-popup").style.display = "flex";
@@ -18,13 +33,22 @@ hbtn.addEventListener("click", function () {
 });
 
 const saveToLocalStorage = () => {
-  localStorage.setItem("inputsName", result.textContent);
+  var newData = document.getElementById("nameInput").value;
+  
+  if (localStorage.getItem("data") == null) {
+    localStorage.setItem("data", "[]");
+  }
+
+  var oldData = JSON.parse(localStorage.getItem("data"));
+  oldData.push(newData);
+
+  localStorage.setItem("data", JSON.stringify(oldData));
 };
 
 lbtn.addEventListener("click", saveToLocalStorage);
 
 lbtn.addEventListener("click", function () {
-  if (lbtn.innerText === "Logga in" && nameInput.value == localStorage.getItem("inputsName")) {
+  if (lbtn.innerText === "Logga in" && nameInput.value == localStorage.getItem("data")) {
     document.querySelector(".background-popup").style.display = "none";
     document.querySelector(".login-again").style.display = "flex";
     document.querySelector(".login-confirm").style.display = "flex";
@@ -42,7 +66,5 @@ cbtn.addEventListener("click", function () {
   document.querySelector(".background-popup").style.display = "none";
 });
 
-nameInput.addEventListener(
-  "input",
-  (letter) => (result.textContent = letter.target.value)
+nameInput.addEventListener("input", (letter) => (result.textContent = letter.target.value)
 );
